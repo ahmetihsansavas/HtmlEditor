a3184a25885f105947268e9fa70895b2d87b3d3b
Ahmet İhsan <ahmetihsan.savas@stu.fsm.edu.tr>
2020-08-20 17:46:49 +0300

    Updates

---
+++ precache-manifest.90a85e7d29cb0f2028d43ceb9a29cf9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf0ce6cd4d342aacebac9452a7d94ff",
+    "url": "/HtmlEditor/index.html"
+  },
+  {
+    "revision": "cd45f925a3be58f4d897",
+    "url": "/HtmlEditor/static/css/2.260df227.chunk.css"
+  },
+  {
+    "revision": "4fb9188aacfbabff1864",
+    "url": "/HtmlEditor/static/css/main.33e516bd.chunk.css"
+  },
+  {
+    "revision": "cd45f925a3be58f4d897",
+    "url": "/HtmlEditor/static/js/2.21799844.chunk.js"
+  },
+  {
+    "revision": "7783785e11cc5ae58f9ad9b1fb9f7e8d",
+    "url": "/HtmlEditor/static/js/2.21799844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb9188aacfbabff1864",
+    "url": "/HtmlEditor/static/js/main.b36687b8.chunk.js"
+  },
+  {
+    "revision": "c98cea04bb98837e3d03",
+    "url": "/HtmlEditor/static/js/runtime-main.480eb97e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HtmlEditor/static/media/logo.5d5d9eef.svg"
+  }
+]);
